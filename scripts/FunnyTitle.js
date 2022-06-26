ec130372fa10e39deecbf2501c6bbb4af88f9ae9
WowYiJiu <wowyijiu@gmail.com>
2022-06-26 15:08:17 +0800

    Site updated: 2022-06-26 15:08:12

---
+++ scripts/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器动态标题
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/WowYiJiu/image@master/blog/favicon.png");
+         document.title = '( •̀ .̫ •́ )✧你要玩捉迷藏嘛';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/WowYiJiu/image@master/blog/favicon.png");
+         document.title = 'This Love！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
